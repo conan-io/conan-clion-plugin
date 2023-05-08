22e0669aea02ffad4e47c9683bd55ca7db4cef0b
GitHub Action <action@github.com>
2023-05-08 17:38:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/rubenrbs/conanclionplugin/listeners/MyFrameStateListener.kt
+++ src/main/kotlin/com/github/rubenrbs/conanclionplugin/listeners/MyFrameStateListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.rubenrbs.conanclionplugin.listeners
 
 import com.intellij.ide.FrameStateListener
 import com.intellij.openapi.diagnostic.thisLogger
