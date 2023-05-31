fff8d63f4ef16ebd3d77f2b077105ece411617ce
czoido <mrgalleta@gmail.com>
2023-05-31 12:17:10 +0200

    add configuration

--- src/main/kotlin/com/jfrog/conan/clionplugin/toolWindow/ConanWindowFactory.kt
+++ src/main/kotlin/com/jfrog/conan/clionplugin/toolWindow/ConanWindowFactory.kt
@@ -9,6 +9,7 @@ import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogPanel
+import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
@@ -18,14 +19,82 @@ import com.intellij.ui.components.JBScrollPane
 import com.intellij.ui.content.ContentFactory
 import com.intellij.ui.table.JBTable
 import com.jfrog.conan.clionplugin.services.MyProjectService
-import java.awt.BorderLayout
-import java.awt.FlowLayout
-import java.awt.Font
-import javax.swing.JButton
-import javax.swing.JLabel
-import javax.swing.JPanel
+import java.awt.*
+import java.io.File
+import java.nio.file.Paths
+import javax.swing.*
 import javax.swing.border.EmptyBorder
 import javax.swing.table.DefaultTableModel
+
+data class ConfigData(val field1: String, val field2: String, val field3: String)
+
+val filePath = Paths.get(System.getProperty("user.home"), "myPluginConfig.txt").toString()
+
+fun loadConfig(): ConfigData {
+    val file = File(filePath)
+    if (!file.exists()) {
+        return ConfigData("", "", "")
+    }
+    val map = file.readLines().associate {
+        val (key, value) = it.split("=")
+        key to value
+    }
+    return ConfigData(map["field1"] ?: "", map["field2"] ?: "", map["field3"] ?: "")
+}
+
+fun saveConfig(data: ConfigData) {
+    val file = File(filePath)
+    val content = "field1=${data.field1}\nfield2=${data.field2}\nfield3=${data.field3}"
+    file.writeText(content)
+}
+
+class MyDialogWrapper : DialogWrapper(true) {
+    val configData = loadConfig()
+    val field1 = JTextField(configData.field1)
+    val field2 = JTextField(configData.field2)
+    val field3 = JTextField(configData.field3)
+
+    init {
+        init()
+        title = "Configuration"
+    }
+
+    override fun createCenterPanel(): JComponent? {
+        val panel = JPanel(GridBagLayout())
+        val gbcLabel = GridBagConstraints().apply {
+            anchor = GridBagConstraints.WEST
+            fill = GridBagConstraints.NONE
+            weightx = 0.0
+            weighty = 0.0
+        }
+        val gbcField = GridBagConstraints().apply {
+            anchor = GridBagConstraints.WEST
+            fill = GridBagConstraints.HORIZONTAL
+            weightx = 1.0
+            weighty = 0.0
+            gridwidth = GridBagConstraints.REMAINDER
+        }
+
+        panel.add(JLabel("Field 1"), gbcLabel)
+        panel.add(field1, gbcField)
+        panel.add(JLabel("Field 2"), gbcLabel)
+        panel.add(field2, gbcField)
+        panel.add(JLabel("Field 3"), gbcLabel)
+        panel.add(field3, gbcField)
+
+        // Use GridBagConstraints to create a placeholder component that will take all remaining vertical space
+        gbcField.weighty = 1.0
+        panel.add(Box.createVerticalGlue(), gbcField)
+
+        return panel
+    }
+
+
+    override fun doOKAction() {
+        saveConfig(ConfigData(field1.text, field2.text, field3.text))
+        super.doOKAction()
+    }
+}
 
 data class Package(val name: String, val version: String)
 
@@ -59,12 +128,7 @@ class ConanWindowFactory : ToolWindowFactory {
                 val actionGroup = DefaultActionGroup().apply {
                     add(object : AnAction("Configure Conan", null, AllIcons.General.Settings) {
                         override fun actionPerformed(e: AnActionEvent) {
-                            Messages.showMessageDialog(
-                                    project,
-                                    "Configure Conan",
-                                    "This will configure Conan",
-                                    Messages.getInformationIcon()
-                            )
+                            MyDialogWrapper().showAndGet()
                         }
                     })
                     add(object : AnAction("Update packages", null, AllIcons.Actions.Refresh) {
