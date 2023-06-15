e6b8329fecb87371f9b185159f711b16632e82c3
czoido <mrgalleta@gmail.com>
2023-06-15 12:43:50 +0200

    wip

--- src/main/kotlin/com/jfrog/conan/clionplugin/dialogs/ConanExecutableDialog.kt
+++ src/main/kotlin/com/jfrog/conan/clionplugin/dialogs/ConanExecutableDialog.kt
@@ -7,10 +7,14 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.openapi.ui.TextFieldWithBrowseButton
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.ui.components.JBCheckBox
+import com.jfrog.conan.clionplugin.cmake.CMake
 import com.jfrog.conan.clionplugin.models.PersistentStorageKeys
 import java.awt.GridBagConstraints
 import java.awt.GridBagLayout
+import java.awt.Insets
 import javax.swing.*
+import javax.swing.border.EmptyBorder
 
 object ConanExecutableChooserDescriptor : FileChooserDescriptor(true, true, false, false, false, false) {
     init {
@@ -28,7 +32,7 @@ val VirtualFile.isConanExecutable: Boolean
         return (extension == null || extension == "exe") && name == "conan"
     }
 
-class ConanExecutableDialogWrapper(project: Project) : DialogWrapper(true) {
+class ConanExecutableDialogWrapper(val project: Project) : DialogWrapper(true) {
     private val properties = project.service<PropertiesComponent>()
 
     private val fileChooserField1 = TextFieldWithBrowseButton().apply {
@@ -55,9 +59,9 @@ class ConanExecutableDialogWrapper(project: Project) : DialogWrapper(true) {
         val panel = JPanel(GridBagLayout())
         val gbcLabel = GridBagConstraints().apply {
             anchor = GridBagConstraints.WEST
-            fill = GridBagConstraints.NONE
             weightx = 0.0
             weighty = 0.0
+            insets = Insets(10, 0, 0, 0)
         }
         val gbcField = GridBagConstraints().apply {
             anchor = GridBagConstraints.WEST
@@ -66,13 +70,55 @@ class ConanExecutableDialogWrapper(project: Project) : DialogWrapper(true) {
             weighty = 0.0
             gridwidth = GridBagConstraints.REMAINDER
         }
+        val gbcCheckboxPanel = GridBagConstraints().apply {
+            anchor = GridBagConstraints.WEST
+            fill = GridBagConstraints.BOTH
+            weightx = 1.0
+            weighty = 1.0
+            gridwidth = GridBagConstraints.REMAINDER
+        }
+        val gbcAutomaticallyAddCheckbox = GridBagConstraints().apply {
+            anchor = GridBagConstraints.WEST
+            fill = GridBagConstraints.HORIZONTAL
+            weightx = 1.0
+            weighty = 0.0
+            gridwidth = GridBagConstraints.REMAINDER
+            insets = Insets(10, 0, 0, 0)
+        }
+        val gbcPlaceholder = GridBagConstraints().apply {
+            fill = GridBagConstraints.VERTICAL
+            weighty = 1.0
+            gridwidth = GridBagConstraints.REMAINDER
+        }
+
         panel.add(JLabel("Conan executable"), gbcLabel)
         panel.add(fileChooserField1, gbcField)
-        panel.add(automaticallyAddCheckbox, gbcField)
 
-        // Use GridBagConstraints to create a placeholder component that will take all remaining vertical space
-        gbcField.weighty = 1.0
-        panel.add(Box.createVerticalGlue(), gbcField)
+        val cmake = CMake(project)
+        val selectedBuildConfiguration = cmake.getSelectedBuildConfiguration()
+
+        val checkboxPanel = JPanel().apply {
+            layout = BoxLayout(this, BoxLayout.Y_AXIS)
+        }
+
+        val configurationsLabel = JLabel("Use Conan for the following configurations:").apply {
+            border = EmptyBorder(10, 0, 0, 0)
+        }
+
+        checkboxPanel.add(configurationsLabel)
+
+        cmake.getActiveProfiles().forEach {
+            // TODO: here we have to check if the dependency provider is already applied to the profile
+            // let the user decide to deactivate it or not for certain profiles
+            val checkbox = JBCheckBox(it.name, it.name == selectedBuildConfiguration?.profileName)
+            checkboxPanel.add(checkbox)
+        }
+
+        panel.add(checkboxPanel, gbcCheckboxPanel)
+
+        panel.add(automaticallyAddCheckbox, gbcAutomaticallyAddCheckbox)
+
+        panel.add(Box.createVerticalGlue(), gbcPlaceholder)
 
         return panel
     }
