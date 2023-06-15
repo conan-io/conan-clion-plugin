78a6eb235d09545f29679657084288d18e85a1b8
czoido <mrgalleta@gmail.com>
2023-06-15 15:19:31 +0200

    implement removeGenerationOptions

--- src/main/kotlin/com/jfrog/conan/clionplugin/cmake/CMake.kt
+++ src/main/kotlin/com/jfrog/conan/clionplugin/cmake/CMake.kt
@@ -34,6 +34,32 @@ class CMake(val project: Project) {
         cmakeSettings.setProfiles(modifiedProfiles)
     }
 
+    fun removeGenerationOptions(profileName: String?, generationOptions: List<String>) {
+        val cmakeSettings = CMakeSettings.getInstance(project)
+        val profiles = cmakeSettings.profiles
+        val modifiedProfiles: MutableList<CMakeSettings.Profile> = mutableListOf()
+
+        for (profile in profiles) {
+            if (profile.name == profileName) {
+                val existingGenerationOptions = profile.generationOptions ?: ""
+                val newGenerationOptions = mutableListOf<String>()
+
+                existingGenerationOptions.split(" ").forEach { option ->
+                    if (!generationOptions.contains(option)) {
+                        newGenerationOptions.add(option)
+                    }
+                }
+
+                val newProfile = profile.withGenerationOptions(newGenerationOptions.joinToString(separator = " "))
+                modifiedProfiles.add(newProfile)
+            } else {
+                modifiedProfiles.add(profile)
+            }
+        }
+
+        cmakeSettings.setProfiles(modifiedProfiles)
+    }
+
     fun getActiveProfiles(): List<CMakeSettings.Profile> {
         return CMakeSettings.getInstance(project).activeProfiles
     }
