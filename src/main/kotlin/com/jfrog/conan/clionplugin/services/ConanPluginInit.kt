f0c08e78ddbb356d6586b0fc3bd3056486b65200
Rubén Rincón <rubenrb@jfrog.com>
2023-06-17 02:21:09 +0200

    Set CMake reload sequentially advanced setting to true on plugin init

--- src/main/kotlin/com/jfrog/conan/clionplugin/services/ConanPluginInit.kt
+++ src/main/kotlin/com/jfrog/conan/clionplugin/services/ConanPluginInit.kt
@@ -3,10 +3,12 @@ package com.jfrog.conan.clionplugin.services
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
 import com.jfrog.conan.clionplugin.conan.ConanPluginUtils
+import com.intellij.openapi.options.advanced.AdvancedSettings
 
 
 class ConanPluginInit : ProjectActivity {
     override suspend fun execute(project: Project) {
         ConanPluginUtils.downloadCMakeProvider()
+        AdvancedSettings.setBoolean("cmake.reload.profiles.sequentially", true)
     }
 }
