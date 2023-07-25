465380a4d7252ad59a821d2fb2a1554e58e7d16d
czoido <mrgalleta@gmail.com>
2023-07-25 21:19:39 +0200

    use StartupActivity

--- src/main/kotlin/com/jfrog/conan/clionplugin/services/ConanPluginInit.kt
+++ src/main/kotlin/com/jfrog/conan/clionplugin/services/ConanPluginInit.kt
@@ -2,11 +2,11 @@ package com.jfrog.conan.clionplugin.services
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.startup.ProjectActivity
+import com.intellij.openapi.startup.StartupActivity
 
 
-class ConanPluginInit : ProjectActivity {
-    override suspend fun execute(project: Project) {
+class ConanPluginInit : StartupActivity {
+    override fun runActivity(project: Project) {
         project.service<ConanService>().downloadLibraryData()
     }
 }
