f86bb5ff6f76b96cb5ef9fb1e387f98c522d2788
czoido <mrgalleta@gmail.com>
2023-07-26 07:54:37 +0200

    update action to match template

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,8 +59,12 @@ qodana {
 }
 
 // Configure Gradle Kover Plugin - read more: https://github.com/Kotlin/kotlinx-kover#configuration
-kover.xmlReport {
-    onCheck = true
+koverReport {
+    defaults {
+        xml {
+            onCheck = true
+        }
+    }
 }
 
 tasks {
