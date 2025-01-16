8d7f916c78394b384f42b586ea688d4f6d879c6c
Abril Rincón Blanco <git@rinconblanco.es>
2025-01-16 12:10:41 +0100

    Fix URL deprecated constructor

--- src/main/kotlin/com/jfrog/conan/clion/conan/extensions/file.kt
+++ src/main/kotlin/com/jfrog/conan/clion/conan/extensions/file.kt
@@ -1,17 +1,14 @@
 package com.jfrog.conan.clion.conan.extensions
 
 import com.intellij.openapi.diagnostic.thisLogger
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import java.io.File
-import java.net.URL
+import java.net.URI
 
 fun File.downloadFromUrl(url: String) {
     val targetFile = this
 
     try {
-        val fileUrl = URL(url)
+        val fileUrl = URI(url).toURL()
         fileUrl.openStream().use { input ->
             targetFile.outputStream().use { output ->
                 input.copyTo(output)
